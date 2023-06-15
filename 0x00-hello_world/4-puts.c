4c6d2e1f8a2d35fdfcd0cb64854628bec5f969e6
Mirry12 <mirfatamir@gmail.com>
2023-06-15 12:59:41 -0700

    task 4 modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#inc lude<stdio.h>
 /**
- * main-Enty point
+ * main - Enty point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
  {
-        puts("Programming is like building a multilingual puzzle);
+        puts("\"Programming is like building a multilingual puzzle");
         returns(0);
- }	
+  }	
