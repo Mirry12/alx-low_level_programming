7fead25ee544ebfffc920eb188d78442f300c6ce
Mirry12 <mirfatamir@gmail.com>
2023-06-16 12:27:03 -0700

    task 9 Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int lowercase = 'a';
+	int upperCase = 'A';
+	while (lowerCase <= 'z')
+	{
+		putchar(loweraCase);
+		lowerCase += 1;
+	}
+	while   (upperCase <= 'z')
+	{
+		putchar (upperCase);
+		upperCase += 1;
+	}
+	putchar ('\n');
+	return (0);
+}
